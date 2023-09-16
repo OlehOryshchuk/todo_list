b05acd61a6b94be7dd4bdcba1dd9a1fbe0b9ec6a
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-16 19:31:54 +0300

    Create TaskUpdateForm with deadline validation

--- todo/form.py
+++ todo/form.py
@@ -20,6 +20,25 @@ class TaskCreateForm(forms.ModelForm):
 
     def clean_deadline(self):
         deadline = self.cleaned_data.get("deadline")
-        if date.today() > deadline:
-            raise ValidationError("Deadline should not be in past!")
-        return deadline
+        return validate_deadline(deadline)
+
+
+class TaskUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "deadline": forms.DateInput(attrs={
+                "type": "date",
+            }),
+        }
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data.get("deadline")
+        return validate_deadline(deadline)
+
+
+def validate_deadline(deadline: date) -> date:
+    if date.today() > deadline:
+        raise ValidationError("Deadline should not be in past!")
+    return deadline
