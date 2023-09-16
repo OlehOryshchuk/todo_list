a6dc0fead16bb9f6ecabb9235dfb7e96819db902
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-16 19:43:39 +0300

    Create URL path for TaskDeleteView

--- todo/urls.py
+++ todo/urls.py
@@ -4,12 +4,13 @@ from .views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
-    # TaskDeleteView
+    TaskDeleteView
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update")
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete")
 ]
 app_name = "todo"
