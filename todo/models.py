1f0902a064f49a590d6ce57443fa21e68e9a95bc
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-16 20:43:17 +0300

    Make string presentation for Tag model

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,9 @@ class Tag(models.Model):
         max_length=100
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255, unique=True)
