05f4c6479e2f5e3a1f52913da92d2397fe68955b
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-09-16 20:45:41 +0300

    Set URL paths for Tag List,Create,Update,Delete

--- todo/urls.py
+++ todo/urls.py
@@ -4,13 +4,23 @@ from .views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
-    TaskDeleteView
+    TaskDeleteView,
+
+    TagListView,
+    TagCreateView,
+    TagDeleteView,
+    TagUpdateView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete")
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+
+    path("tags", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
 ]
 app_name = "todo"
